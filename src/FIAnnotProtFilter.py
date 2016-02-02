7d1a979df0bd04c07cbe6a40bc19bef441a35390
Joshua Burkhart <burkhart.joshua@gmail.com>
2016-02-02 13:38:14 -0800

    storing uniprot Swiss-Prot id's, working on Interactome3D query

--- src/FIAnnotProtFilter.py
+++ src/FIAnnotProtFilter.py
@@ -1,12 +1,17 @@
 import re
 import mygene
+import urllib.request
+import urllib.parse
+import xml
 
 GENE_DATA_ROW = '^(?P<gene1>[A-Z]+[0-9A-Z]+)\s+(?P<gene2>[A-Z]+[0-9A-Z]+)'
 
 Gene_set = set()
 FI_set = set()
 Pdb_dict = dict()
-AnnotProt_list = list()
+Uni_dict = dict()
+AnnotPdb_list = list()
+AnnotUni_list = list()
 
 in_fptr = open("../data/input/FIsInGene_121514_with_annotations.txt")
 while 1:
@@ -34,8 +39,14 @@ for gene in annot_gene_list:
     except KeyError:
         # ignore genes without PDB's
         pass
+    try:
+        Uni_dict[gene['query']] = gene['uniprot']['Swiss-Prot']
+    except KeyError:
+        # ignore genes without uniprot's
+        pass
 
-[AnnotProt_list.append((gene_tup[0],
+
+[AnnotPdb_list.append((gene_tup[0],
                         Pdb_dict[gene_tup[0]],
                         gene_tup[1],
                         Pdb_dict[gene_tup[1]]))
@@ -43,10 +54,53 @@ for gene in annot_gene_list:
  if gene_tup[0] in Pdb_dict and
     gene_tup[1] in Pdb_dict]
 
-out_fptr = open("../data/output/FIAnnotProt.txt",'w+')
+[AnnotUni_list.append((gene_tup[0],
+                        Uni_dict[gene_tup[0]],
+                        gene_tup[1],
+                        Uni_dict[gene_tup[1]]))
+ for gene_tup in FI_set
+ if gene_tup[0] in Uni_dict and
+    gene_tup[1] in Uni_dict]
+
+out_fptr = open("../data/output/FIAnnotPdb.txt",'w+')
 [out_fptr.write("{0},{1},{2},{3}\n".format(item[0],
                                            item[1],
                                            item[2],
                                            item[3]))
- for item in sorted(AnnotProt_list)]
+ for item in sorted(AnnotPdb_list)]
 out_fptr.close()
+
+out_fptr = open("../data/output/FIAnnotUni.txt",'w+')
+[out_fptr.write("{0},{1},{2},{3}\n".format(item[0],
+                                           item[1],
+                                           item[2],
+                                           item[3]))
+ for item in sorted(AnnotUni_list)]
+out_fptr.close()
+
+#not yet implemented, bail out
+exit(1)
+for line in AnnotUni_list:
+    gene1 = line[0]
+    unip1 = line[1]
+    gene2 = line[2]
+    unip2 = line[3]
+
+    query_data = {}
+    query_data['queryProt1'] = unip1
+    query_data['queryProt2'] = unip2
+
+    url_values = urllib.parse.urlencode(query_data)
+
+    url = 'http://interactome3d.irbbarcelona.org/api/getInteractionStructures'
+    full_url = '{0}?{1}'.format(url,url_values)
+
+    #TODO: Parse the XML
+    #https://docs.python.org/3/library/xml.etree.elementtree.html
+    #http://interactome3d.irbbarcelona.org/help.php#restful
+    #http://interactome3d.irbbarcelona.org/help.php#interactions_dat_file
+
+    with urllib.request.urlopen(full_url) as response:
+        html = response.read()
+        xmlTree = xml.etree.ElementTree.parse(html)
+        xmlRoot = root = xmlTree.getroot()
