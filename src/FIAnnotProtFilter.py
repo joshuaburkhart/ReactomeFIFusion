3ffe43a1d02350fcb593981f55d444f863e8bec2
Joshua Burkhart <burkhart.joshua@gmail.com>
2016-02-04 20:34:07 -0800

    now querying Interactome3D database for known interactions between genes

--- src/FIAnnotProtFilter.py
+++ src/FIAnnotProtFilter.py
@@ -2,7 +2,7 @@ import re
 import mygene
 import urllib.request
 import urllib.parse
-import xml
+import xml.etree.ElementTree
 
 GENE_DATA_ROW = '^(?P<gene1>[A-Z]+[0-9A-Z]+)\s+(?P<gene2>[A-Z]+[0-9A-Z]+)'
 
@@ -45,24 +45,24 @@ for gene in annot_gene_list:
         # ignore genes without uniprot's
         pass
 
-
 [AnnotPdb_list.append((gene_tup[0],
-                        Pdb_dict[gene_tup[0]],
-                        gene_tup[1],
-                        Pdb_dict[gene_tup[1]]))
+                       Pdb_dict[gene_tup[0]],
+                       gene_tup[1],
+                       Pdb_dict[gene_tup[1]]))
  for gene_tup in FI_set
  if gene_tup[0] in Pdb_dict and
-    gene_tup[1] in Pdb_dict]
+ gene_tup[1] in Pdb_dict]
 
 [AnnotUni_list.append((gene_tup[0],
-                        Uni_dict[gene_tup[0]],
-                        gene_tup[1],
-                        Uni_dict[gene_tup[1]]))
+                       Uni_dict[gene_tup[0]],
+                       gene_tup[1],
+                       Uni_dict[gene_tup[1]]))
  for gene_tup in FI_set
  if gene_tup[0] in Uni_dict and
-    gene_tup[1] in Uni_dict]
+ gene_tup[1] in Uni_dict]
 
-out_fptr = open("../data/output/FIAnnotPdb.txt",'w+')
+# Print Genes With PDB's to File
+out_fptr = open("../data/output/FIAnnotPdb.txt", 'w+')
 [out_fptr.write("{0},{1},{2},{3}\n".format(item[0],
                                            item[1],
                                            item[2],
@@ -70,7 +70,8 @@ out_fptr = open("../data/output/FIAnnotPdb.txt",'w+')
  for item in sorted(AnnotPdb_list)]
 out_fptr.close()
 
-out_fptr = open("../data/output/FIAnnotUni.txt",'w+')
+# Print Genes With UNIPROT: Swiss-Prot's to File
+out_fptr = open("../data/output/FIAnnotUni.txt", 'w+')
 [out_fptr.write("{0},{1},{2},{3}\n".format(item[0],
                                            item[1],
                                            item[2],
@@ -78,13 +79,31 @@ out_fptr = open("../data/output/FIAnnotUni.txt",'w+')
  for item in sorted(AnnotUni_list)]
 out_fptr.close()
 
-#not yet implemented, bail out
-exit(1)
-for line in AnnotUni_list:
+# Print Genes With PDB's to File With Their UNIPROT: Swiss-Prot's
+out_fptr = open("../data/output/FIAnnotPdbWithUni.txt", 'w+')
+for item in sorted(AnnotPdb_list):
+    try:
+        out_fptr.write("{0},{1},{2},{3}\n".format(item[0],
+                                                  Uni_dict[item[0]],
+                                                  item[2],
+                                                  Uni_dict[item[2]]))
+    except KeyError:
+        # ignore genes without both PDB's and Swiss-Prot's
+        pass
+out_fptr.close()
+
+intr_count = 0
+non_intr_count = 0
+except_count = 0
+loop_count = 0
+loop_len = len(AnnotPdb_list)
+out_fptr = open("../data/output/FIInteract.txt", 'w+')
+for line in AnnotPdb_list:
+    loop_count += 1
     gene1 = line[0]
-    unip1 = line[1]
+    unip1 = Uni_dict[line[0]]
     gene2 = line[2]
-    unip2 = line[3]
+    unip2 = Uni_dict[line[2]]
 
     query_data = {}
     query_data['queryProt1'] = unip1
@@ -93,14 +112,43 @@ for line in AnnotUni_list:
     url_values = urllib.parse.urlencode(query_data)
 
     url = 'http://interactome3d.irbbarcelona.org/api/getInteractionStructures'
-    full_url = '{0}?{1}'.format(url,url_values)
+    full_url = '{0}?{1}'.format(url, url_values)
 
-    #TODO: Parse the XML
-    #https://docs.python.org/3/library/xml.etree.elementtree.html
-    #http://interactome3d.irbbarcelona.org/help.php#restful
-    #http://interactome3d.irbbarcelona.org/help.php#interactions_dat_file
+    print("Querying interaction between {0} and {1} ({2} of {3})...".format(
+        gene1,
+        gene2,
+        loop_count,
+        loop_len
+    ))
 
-    with urllib.request.urlopen(full_url) as response:
-        html = response.read()
-        xmlTree = xml.etree.ElementTree.parse(html)
-        xmlRoot = root = xmlTree.getroot()
+    try:
+        with urllib.request.urlopen(full_url) as response:
+            html = response.read()
+
+        xmlTree = xml.etree.ElementTree.fromstring(html)
+        if len(xmlTree.getchildren()[0].getchildren()) > 0:
+            intr_msg = 'Interaction Reported by Interactome3D,{0}'.format(
+                full_url
+            )
+            intr_count += 1
+        else:
+            intr_msg = 'No Interaction Reported by Interactome3D,'
+            non_intr_count += 1
+    except Exception as e:
+        intr_msg = 'Exception Raised: {0},'.format(e)
+        except_count += 1
+    out_fptr.write("{0},{1},{2},{3},{4}\n".format(
+        gene1,
+        unip1,
+        gene2,
+        unip2,
+        intr_msg
+    ))
+out_fptr.close()
+print('Interaction Count: {0}\n' \
+      'Non Interaction Count: {1}\n' \
+      'Exception Count: {2}'.format(
+    intr_count,
+    non_intr_count,
+    except_count
+))
